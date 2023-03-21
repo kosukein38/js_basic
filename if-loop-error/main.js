fc38b8da444df5121d4207302947fcf8e0e8cfef
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-03-21 21:58:30 +0900

    [Finish] until section4-57

---
+++ if-loop-error/main.js
@@ -0,0 +1,14 @@
+let ok = true;
+let maybeOk = false;
+
+// if (ok) {
+//   console.log('OK!');
+// } else if (maybeOk) {
+//   console.log('maybe OK...');
+// } else {
+//   console.log('NO!');
+// }
+const coffee1 = { name: 'Cafe Latte' };
+const coffee2 = { name: 'Cafe Latte' };
+ok = coffee1 === coffee2;
+console.log(ok);
