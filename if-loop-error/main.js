07029a83bf625d4d77dd285057eb6a7116982765
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-03-23 21:17:03 +0900

    [Finish] until section4-64

--- if-loop-error/main.js
+++ if-loop-error/main.js
@@ -8,7 +8,56 @@ let maybeOk = false;
 // } else {
 //   console.log('NO!');
 // }
-const coffee1 = { name: 'Cafe Latte' };
-const coffee2 = { name: 'Cafe Latte' };
-ok = coffee1 === coffee2;
-console.log(ok);
+// const coffee1 = { name: 'Cafe Latte' };
+// const coffee2 = { name: 'Cafe Latte' };
+// ok = coffee1 === coffee2;
+// console.log(ok);
+
+// ok = !true;
+// ok = !!'';
+// console.log(ok);
+
+const hello = 'hello2';
+{
+  //console.log(helllo); //エラーが起きる「デッドゾーン」
+  const hello = 'hello1';
+}
+
+// ok = false;
+// ok = ok ? 'OK' : 'NO';
+// console.log(ok);
+
+function vegetableColor(vegetable) {
+  switch (vegetable) {
+    case 'tomato': {
+      const message = 'tomato is red!';
+      console.log(message);
+      break;
+    }
+    case 'carrot':
+    case 'pumpkin': {
+      const message = `${vegetable} is orange!`;
+      console.log(message);
+      break;
+    }
+    case 'onion': {
+      const message = 'onion is white!';
+      console.log(message);
+      break;
+    }
+    default: {
+      const message = 'not found';
+      console.log(message);
+      break;
+    }
+  }
+  // if (vegetable === 'tomato') {
+  //   console.log('tomato is red!');
+  // } else if (vegetable === 'pumpkin') {
+  //   console.log('pumpkin is orange!');
+  // } else if (vegetable === 'onion') {
+  //   console.log('onion is white!');
+  // }
+}
+
+vegetableColor('carrot');
