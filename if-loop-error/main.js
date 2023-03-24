cdba810687c922ee0e95b8fe60aa3771343265d3
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-03-24 21:29:50 +0900

    [Finish] until section4-70

--- if-loop-error/main.js
+++ if-loop-error/main.js
@@ -27,37 +27,66 @@ const hello = 'hello2';
 // ok = ok ? 'OK' : 'NO';
 // console.log(ok);
 
-function vegetableColor(vegetable) {
-  switch (vegetable) {
-    case 'tomato': {
-      const message = 'tomato is red!';
-      console.log(message);
-      break;
-    }
-    case 'carrot':
-    case 'pumpkin': {
-      const message = `${vegetable} is orange!`;
-      console.log(message);
-      break;
-    }
-    case 'onion': {
-      const message = 'onion is white!';
-      console.log(message);
-      break;
-    }
-    default: {
-      const message = 'not found';
-      console.log(message);
-      break;
-    }
-  }
-  // if (vegetable === 'tomato') {
-  //   console.log('tomato is red!');
-  // } else if (vegetable === 'pumpkin') {
-  //   console.log('pumpkin is orange!');
-  // } else if (vegetable === 'onion') {
-  //   console.log('onion is white!');
-  // }
+// function vegetableColor(vegetable) {
+//   switch (vegetable) {
+//     case 'tomato': {
+//       const message = 'tomato is red!';
+//       console.log(message);
+//       break;
+//     }
+//     case 'carrot':
+//     case 'pumpkin': {
+//       const message = `${vegetable} is orange!`;
+//       console.log(message);
+//       break;
+//     }
+//     case 'onion': {
+//       const message = 'onion is white!';
+//       console.log(message);
+//       break;
+//     }
+//     default: {
+//       const message = 'not found';
+//       console.log(message);
+//       break;
+//     }
+//   }
+// if (vegetable === 'tomato') {
+//   console.log('tomato is red!');
+// } else if (vegetable === 'pumpkin') {
+//   console.log('pumpkin is orange!');
+// } else if (vegetable === 'onion') {
+//   console.log('onion is white!');
+//   }
+// }
+
+// vegetableColor('carrot');
+
+let count = 100;
+while (count < 10) {
+  console.log('while: ', count);
+  count += 1;
 }
 
-vegetableColor('carrot');
+let tomatoCount = 100;
+do {
+  console.log('do-while: ', tomatoCount);
+  tomatoCount += 1;
+} while (tomatoCount < 10);
+
+for (
+  let pumpkinCount = 0, i = 0;
+  pumpkinCount < 10;
+  pumpkinCount += 1, i += 1
+) {
+  // console.log(pumpkinCount, i);
+}
+
+const fruits = ['apple', 'banana', 'grape', 'ogange', 'mango'];
+for (let i = 0; i < fruits.length; i += 1) {
+  console.log(fruits[i]);
+}
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
