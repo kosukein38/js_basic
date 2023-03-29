bf297318b890a857067e65e30018d9960d85d331
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-03-29 21:41:35 +0900

    [Finish] until section6-95

---
+++ internal/main.js
@@ -0,0 +1,13 @@
+'use strict';
+var hello = 'hello';
+var hello = 'hi';
+console.dir(sayTomato);
+
+{
+  function sayTomato() {
+    var tomato = 'tomato';
+  }
+}
+console.dir(sayTomato);
+var apple = 'apple';
+console.log(globalThis);
