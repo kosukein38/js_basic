5e7fd9c026b624fea46a10c0503774a1245727f0
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-03-30 21:46:29 +0900

    [Finish] until section6-98

--- internal/main.js
+++ internal/main.js
@@ -1,13 +1,23 @@
 'use strict';
 var hello = 'hello';
 var hello = 'hi';
-console.dir(sayTomato);
+// console.dir(sayTomato);
 
 {
   function sayTomato() {
     var tomato = 'tomato';
   }
 }
-console.dir(sayTomato);
+// console.dir(sayTomato);
 var apple = 'apple';
 console.log(globalThis);
+
+const coffee = {
+  name: 'Caffe Latte',
+};
+// const coffee2 = coffee;
+// coffee2.name = 'Espresso';
+const coffee3 = {
+  name: 'Caffe Latte',
+};
+console.log(coffee === coffee3);
